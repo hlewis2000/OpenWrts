7540f1e9144058f0b23b8673d233b2ff2ff7522c
hlewis2000 <152189953+hlewis2000@users.noreply.github.com>
2024-04-03 09:22:51 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Modify default system settings
 
-# 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+# 修改默认IP为192.168.5.1
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate 
 
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -15,7 +15,13 @@ echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-pa
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
 # iStore
+# echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
+# iStore
+echo >> feeds.conf.default
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
 
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
